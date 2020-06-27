746d45c6decc65169920d8af9c9789b882d4f757
Guilherme Lucio Silva <guilhermelucio.1811@gmail.com>
2020-06-27 19:34:26 -0300

    Create ordem_nao_crescente.py

---
+++ Brincando_com_comandos/ordem_nao_crescente.py
@@ -0,0 +1,11 @@
+A = [31, 41, 59, 26, 41, 22, 19]
+for chave, valor in enumerate(A):
+    i = chave
+    j = i - 1
+    while i > 0 and A[i] > A[j]:
+        A[i] = A[j]
+        A[j] = valor
+        i -= 1
+        j -= 1
+
+print(A)
